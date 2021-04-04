cf78bac32e4ce80fdb25ed241dca61663b3a377e
mayukuse24 <vinaypoll@gmail.com>
2021-04-03 19:48:47 -0500

    Added action simulation to simulate car variables

--- main.pl
+++ main.pl
@@ -1,13 +1,13 @@
-:- include(scenarios/scenario_1).
+%:- include(scenarios/scenario_1).
 
-%start_drive(Scenario_file, Timestamp) :- consult(Scenario_file), start_drive(Timestamp).
-start_drive(Timestamp) :- suggest_decision(Timestamp, Action), Timestamp1 is Timestamp + 1, start_drive(Timestamp1).
+start_drive(Scenario_file, Timestamp) :- consult(Scenario_file), start_drive(Timestamp).
+start_drive(Timestamp) :- suggest_decision(Timestamp, Action), execute(Action, Timestamp), Timestamp1 is Timestamp + 1, start_drive(Timestamp1).
 
 % TODO: consider using something like "choice rules" in CLINGO (see AV paper)
-% to select one of n possible actions for each factI have
+% to select one of n possible actions for each fact
 
-suggest_decision(T, brake) :- brake_constraints(T), writeln(brake(T)), sleep(0.5).
-suggest_decision(T, accelerate) :- acc_constraints(T), writeln(accelerate(T)), sleep(0.5).
+suggest_decision(T, brake) :- brake_constraints(T), writeln(brake(T)), sleep(0.5), !.
+suggest_decision(T, accelerate) :- acc_constraints(T), writeln(accelerate(T)), sleep(0.5), !.
 suggest_decision(T, none) :- writeln(none(T)), sleep(0.5).
 
 acc_constraints(T) :- below_speed_limit(T), not(obstacle_in_front(T)).
@@ -17,3 +17,9 @@ brake_constraints(T) :- traffic_signal(red, T).
 
 obstacle_in_front(T) :- self_lane(Lid, T), lane(Lid, Objs, T), member(Oid, Objs), depth(Oid, D, T), D < 10.
 below_speed_limit(T) :- self_speed(S, T), speed_limit(SL, T), S =< SL.
+
+
+% Simulate chosen actions
+execute(accelerate, T) :- self_speed(S, T), S1 is S + 1, T1 is T + 1, assertz(self_speed(S1, T1)).
+execute(brake, T) :- self_speed(S, T), S1 is S - 1, T1 is T + 1, assertz(self_speed(S1, T1)).
+execute(none, T) :- self_speed(S, T), T1 is T + 1, assertz(self_speed(S, T1)).
--- scenarios/scenario_1.pl
+++ scenarios/scenario_1.pl
@@ -1,7 +1,11 @@
 
+:- abolish(self_speed/2).
+:- dynamic self_speed/2.
+:- dynamic self_lane/2.
+
 %%% FRAME 1 %%%
 % self_speed(speed, timestamp).
-self_speed(10, 1).
+self_speed(5, 1).
 
 % self_lane(lane id, timestamp).
 self_lane(1, 1).
@@ -25,9 +29,8 @@ traffic_signal(none, 1).
 
 
 %%% FRAME 2 %%%
-
-self_speed(10, 2).
 self_lane(1, 2).
+
 speed_limit(10, 2).
 
 depth(1, 20, 2).
@@ -38,3 +41,45 @@ lane(2, [], 2).
 lane(3, [], 2).
 
 traffic_signal(none, 2).
+
+%%% FRAME 3 %%%
+self_lane(1, 3).
+
+speed_limit(10, 3).
+
+depth(1, 15, 3).
+depth(2, 37, 3).
+
+lane(1, [1, 2], 3).
+lane(2, [], 3).
+lane(3, [], 3).
+
+traffic_signal(none, 3).
+
+%%% FRAME 4 %%%
+self_lane(1, 4).
+
+speed_limit(10, 4).
+
+depth(1, 9, 4).
+depth(2, 41, 4).
+
+lane(1, [1, 2], 4).
+lane(2, [], 4).
+lane(3, [], 4).
+
+traffic_signal(none, 4).
+
+%%% FRAME 5 %%%
+self_lane(1, 5).
+
+speed_limit(10, 5).
+
+depth(1, 7, 5).
+depth(2, 40, 5).
+
+lane(1, [1, 2], 5).
+lane(2, [], 5).
+lane(3, [], 5).
+
+traffic_signal(none, 5).
